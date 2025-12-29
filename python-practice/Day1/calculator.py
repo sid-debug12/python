30d6294ed6babb35c5bf0465ad79fa8affb69cef
sid-debug12 <ntechosigfrird@gmail.com>
2025-12-29 19:53:46 +0100

    Add files via upload

---
+++ python-practice/Day1/calculator.py
@@ -0,0 +1,31 @@
+num1 = input("Enter the first number : ")
+print(type(num1))
+
+num1 = int(num1)
+print(type(num1))
+
+num2 = input("Enter the second number : ")
+print(type(num2))
+
+num2 = int(num2)
+print(type(num2))
+
+numb = num1 + num2
+nume = num1 * num2
+numr = num1 - num2
+numt = num1 / num2
+num3 = num1 // num2
+num4 = round(num1 / num2)
+num5 = num1 ** num2
+num6 = num1 is num2
+num7 = num1 % num2
+
+print(numb)
+print(nume)
+print(numr)
+print(numt)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
