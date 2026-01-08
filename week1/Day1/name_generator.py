ee041e549950344c9945fdad712f01d5899205c7
sid-debug12 <ntechosigfrird@gmail.com>
2026-01-08 08:54:51 +0100

    Add files via upload

---
+++ week1/Day1/name_generator.py
@@ -0,0 +1,8 @@
+# Name Tag generator
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+
+print("Hello, " + first_name + " " + last_name +
+      ". You are " + str(age) + " years old.")
---
+++ week1/Day1/simple_calculator.py
@@ -0,0 +1,14 @@
+# Simple calculator
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+total_sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+division = first_number / second_number
+
+print("The sum is " + str(total_sum))
+print("The difference is " + str(difference))
+print("The product is " + str(product))
+print("The division is " + str(division))
